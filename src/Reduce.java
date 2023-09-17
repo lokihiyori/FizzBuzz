67db12c7357eedef94a84db7fdde1f26f4407daa
Kaisheng liu <kaisheng.liu@mail.utoronto.ca>
2023-09-17 18:24:30 -0400

    tut1 reduce

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int count = 0;
+        int i = 100;
+        while ( i > 0) {
+            if (i % 2 == 0) {
+                i = i / 2;
+                count++;
+            }
+            if (i % 2 == 1) {
+                i = i - 1;
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
