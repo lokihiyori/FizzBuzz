722323098e23c804872f8123d8839f021d637d78
Kaisheng liu <kaisheng.liu@mail.utoronto.ca>
2023-09-17 17:57:50 -0400

    tut1 readme

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,7 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main() {
         int i = 1;
         while (i < 100) {
 
@@ -14,7 +14,7 @@ class FizzBuzz {
         }
     }
 
-    private static void dofizzgame(int i) {
+    public static void dofizzgame(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.dofizzgame(5);
+        FizzBuzz.main();
+
+    }
+}
