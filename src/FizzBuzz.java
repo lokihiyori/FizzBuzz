33d6385e1fe07b1a4c393c2f19b6578c058232ea
Kaisheng liu <kaisheng.liu@mail.utoronto.ca>
2024-05-16 18:55:47 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,15 +4,40 @@
 class FizzBuzz {
 
     public static void main() {
+
         int i = 1;
+
         while (i < 100) {
 
-            // Find out which numbers divide i.
-            dofizzgame(i);
-            i ++;
+            dofizz(i);
+
+            i++;
+        }
+    }
+
+    private static void dofizz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
 
         }
     }
+
 
     public static void dofizzgame(int i) {
         boolean divisibleBy3 = i % 3 == 0;
